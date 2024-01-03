7952b3ec2a6430a15ff1119b725da6c867d804be
anne <anneirungu07@gmail.com>
2024-01-03 21:17:33 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+//saturday fun
+function saturdayFun(activity='swimming', company='my friends', place='in the ocean'){
+    console.log(`This Saturday, I want to go ${activity} with ${company} ${place}`)
+}
+saturdayFun()
+
+//Monday work
+function mondayWork(task='go meet the client') {
+    console.log(`This Monday, I will ${task}`)
+}
+mondayWork()
